a6c8d979bb5f41a1575eb5112ec10f2edf5b0162
endstar212 <rootkadir55@gmail.com>
2024-05-22 20:15:53 +0000

    duzenlemer

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const axios = require('axios');
 const fs = require('fs');
-let data = JSON.stringify({
-  "prompt": "biomechanical style illustration beautiful woman, colorful melting human head. acrylic bubbles and flowers, ferrofluids, water distortions. looking up, intricate abstract. intricate artwork. beeple. blend of organic and mechanical elements, futuristic, cybernetic, detailed, intricate, dark theme, in awe:1.2, confident, surprised:0.7, natural skin, style of Stan Manoukian"
-});
+const express = require('express');
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'http://ai.test/api/create-image',
-  headers: { 
-    'Authorization': '019ldkfgDjSCElbYVWSzZXqvTYWRtaW5AdGVzdC5jb21SVXlEUll1MmQ=', 
-    'Content-Type': 'application/json'
-  },
-  data : data
-};
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-    let base64Image = response.data;
-    let base64ImageString = base64Image.split(';base64,').pop();
-    fs.writeFile('image.png', base64ImageString, {encoding: 'base64'}, function(err) {
-      console.log('File created');
-    });
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  let data = JSON.stringify({
+    "prompt": "uçan araba"
+  });
+  
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://httpbin.org/get',
+    data : data
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    res.send(response.data.url)
+  })
+  .catch((error) => {
+    res.send(error)
+  });
+  
 })
-.catch((error) => {
-  console.log(error);
-});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
